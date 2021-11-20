39247957d199aac96acaf6b112801bb639a18d21
Kenny <54686876+Kenny-star@users.noreply.github.com>
2021-11-20 01:04:27 -0500

    tired

--- webApp.py
+++ webApp.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify, abort
 import requests
 import connection
-import Flask
+from flask import Flask
 
 
 myHeader = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,' 'image/webp,/;q=0.8',
