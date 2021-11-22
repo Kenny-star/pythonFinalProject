4fc881a1a80f79388a6ef29524635c19e55dc8b4
Kenny <54686876+Kenny-star@users.noreply.github.com>
2021-11-22 14:28:30 -0500

    I think i've fixed the issue frl frl frl

--- webApp.py
+++ webApp.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 myHeader = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,' 'image/webp,/;q=0.8',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 ' 'Firefox/93.0',
-            'Content-Type': 'application/json'}
+            }
 
 app = Flask(__name__)
 
