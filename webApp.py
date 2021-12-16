572da374499fd86ae60833e274cf7c38f742748b
Kenny <54686876+Kenny-star@users.noreply.github.com>
2021-12-16 16:19:31 -0500

    Changed the post format

--- webApp.py
+++ webApp.py
@@ -32,23 +32,24 @@ def add_Log_Temp():
     conn = connection.connect()
     mycursor = conn.cursor()
     query = "INSERT INTO final (Temperature, Humidity, Timestamp1) VALUES (%s, %s, %s);"
-    query2 = "INSERT INTO piunit (pi_valueunit) VALUES (%s);"
+    query2 = "INSERT INTO piunit (pi_valuecelsius, pi_valuefahrenheit) VALUES (%s);"
     query3 = "INSERT INTO pival (value_temp, value_hum, timestamp_) VALUES (%s, %s, %s);"
     query4 = "INSERT INTO summary (pi_value) VALUES (%s);"
 
     temp_data = request.json["Temperature"]
+    fah_data = request.json["Fahrenheit"]
     hum_data = request.json["Humidity"]
     time_data = request.json["Timestamp1"]
 
     val = (temp_data, hum_data, time_data)
-    val2 = (temp_data, hum_data, time_data)
+    val2 = (temp_data, fah_data)
     val3 = (temp_data, hum_data, time_data)
-    val4 = (temp_data, hum_data, time_data)
+    val4 = temp_data
 
     mycursor.execute(query, val)
-    mycursor.execute(query2, val)
-    mycursor.execute(query3, val)
-    mycursor.execute(query4, val)
+    mycursor.execute(query2, val2)
+    mycursor.execute(query3, val3)
+    mycursor.execute(query4, val4)
 
     conn.commit()
     mycursor.close()
